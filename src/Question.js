c5adbb14a6c4a40e0e6bf710e992973ce0a45fb2
Andres Pena <penchjpc@gmail.com>
2022-05-17 16:03:28 -0400

    successfully rendered questions with list of options in different order

--- src/Question.js
+++ src/Question.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-export default function Question(props) {
+export default function Question({data}) {
     console.log('question mounted')
-    function rndmOrder(){
+     // generates an array with 4 numbers between 0 and 3, in random order.
+    function randomOrder(){
         const indexArr = []
         while(indexArr.length < 4){
           const num = Math.floor(Math.random()*4)
@@ -12,7 +13,24 @@ export default function Question(props) {
         }
         return indexArr
       }
+      // Array with 4 numbers between 0 and 3, in random order
+    const arr = randomOrder()
+      // Array that will hold all options, populated first with the incorrect answers
+    const options = data.incorrect_answers
+    // Correct answer pushed into the array of all options
+    options.push( data.correct_answer)
+
   return (
-    <div> {props.data.question} </div>
+    <div>
+      <h3> {data.question} </h3> 
+      {/* Random values of [arr] allow to retrieve values of [options] in different order */}
+      <ul>        
+        <li> { options[ arr[0] ] } </li>
+        <li> { options[ arr[1] ] } </li>
+        <li> { options[ arr[2] ] } </li>
+        <li> { options[ arr[3] ] } </li>
+      </ul>
+      <hr></hr>
+    </div>
   )
 }
